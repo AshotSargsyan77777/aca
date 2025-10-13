1b74866a4c2791b8ce135eb9e250cbd4cfe210d3
AshotSargsyan7777 <ashotsargsyan7412@gmail.com>
2025-10-14 02:26:23 +0400

    aaa

---
+++ aca.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void my_malloc(std::size_t size, void** ptr) {
+    *ptr = malloc(size);
+    if (!(*ptr)) {
+        *ptr = nullptr;
+    }
+}
+int main() {
+    int* ptr;
+    my_malloc(5 * sizeof(int), (void**)&ptr);
+    if (!ptr) {
+        std::cout << "NO" << std::endl;
+    } else {
+        std::cout << "Memory allocated successfully!" << std::endl;
+    }
+}
